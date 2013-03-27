3d870a5d499456529a0908728b07c5ccb9fae51a
pattraynor <ptraynor@asu.edu>
2013-03-27 12:34:29 -0700

    Initial File for Medical Record

---
+++ Medical_Record/src/NewMain.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Patrick Traynor
+ */
+public class NewMain 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("They think I'm Dumb");
+        // TODO code application logic here
+    }
+}
