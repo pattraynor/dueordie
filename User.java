09a704dafb35d64c7d5ed0df0739ca7e1f382a3c
hiddenmonkey <boogaloofa@gmail.com>
2013-04-01 17:23:39 -0700

    Update User.java

--- User.java
+++ User.java
@@ -1,13 +1,21 @@
 public class User {
-  private String userName;
+	private String userName;
 	private String userType;
-	private String[] answers;
+	private String answer1;
+	private String answer2; 
+	private String answer3;
 	private int pin;
 	private int userID;
 
 
-public String[] getAnswers(){
-	return answers;
+public String getAnswer1(){
+	return answer1;
+}
+public String getAnswer2(){
+	return answer2;
+}
+public String getAnswer3(){
+	return answer3;
 }
 public void setUserID(int newID){
 	userID = newID;
