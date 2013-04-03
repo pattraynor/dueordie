00ea5cfc6a17648f2d1f9750e45de62bd6f1b360
scarysally <rnapper@asu.edu>
2013-04-03 15:18:40 -0700

    Update Visit.java

--- Visit.java
+++ Visit.java
@@ -1,7 +1,9 @@
+
 public class Visit 
 {
-  private int year;
-	private int date;
+
+	private int year;
+	private int day;
 	private int month;
 	private double bloodPressure;
 	private double sugar;
@@ -11,5 +13,41 @@ public class Visit
 	private String prescriptions;
 	private String symptoms;
 	private String notes;
+	
+	public int getDay()
+	{
+		return day;
+	}
+	
+	public int getMonth()
+	{
+		return month;
+	}
+	
+	public int getYear()
+	{
+		return year;
+	}
+	
+	public void setDay(int date)
+	{
+		day = date;
+	}
+	
+	public void setMonth(int newMonth)
+	{
+		month = newMonth;
+	}
+	
+	public void setYear(int newYear)
+	{
+		year = newYear;
+	}
+	
+	public void setNotes(String newNotes)
+	{
+		notes = newNotes;
+	}
+	
 	
 }
