3ab851b024a931e500626ced4e9638b14f32f198
pattraynor <ptraynor@asu.edu>
2013-04-03 15:43:36 -0700

    Upload Medical_record file mark crusaders yay!

--- Medical_Record/src/MedicalRecord.java
+++ Medical_Record/src/MedicalRecord.java
@@ -1,12 +1,57 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Class to be implemented with Visit.
  */
 
 /**
  *
  * @author Patrick Traynor
  */
-public class MedicalRecord {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MedicalRecord 
+{
+    protected Node head;
+    protected int listSize;
+    protected int visitCount;
+    
+    //ArrayLists to Implement for charts:_____________________________________
+    protected ArrayList<Double> sugarChart = new ArrayList<Double>();
+                // How to add into double arraylist: 
+                // intList.add(1);
+                // intList.add(2);
+    protected ArrayList<Double> weightChart = new ArrayList<Double>();
+    protected ArrayList<Double> heightChart = new ArrayList<Double>();
+    protected ArrayList<Double> temperatureChart = new ArrayList<Double>();
+    protected ArrayList<Double> bloodPressureChart = new ArrayList<Double>();
+    protected ArrayList<String> prescriptions = new ArrayList<String>();
+    //___________________________________________________________________
+    public MedicalRecord()
+    {
+        head = null;        
+        listSize = 0;
+        visitCount = 0;
+    }
+    
+    public Visit getLastVisit()
+    {
+       
+        return null;
+    }
+    public Visit getVisit(int year, int month, int day)
+    {
+        
+        return 
+    }
+    public int getVisitCount()
+    {  
+        return visitCount;
+        
+    }
+    public Arraylist getSugarChart()
+    {
+        return ..;
+    }
+    public
     
 }
--- Medical_Record/src/NewMain.java
+++ Medical_Record/src/NewMain.java
@@ -12,6 +12,7 @@ public class NewMain
 
     public static void main(String[] args) 
     {
+                
         System.out.println("They think I'm Dumb");
         // TODO code application logic here
     }
