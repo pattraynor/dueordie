b8afb14a1ba80726c13fce7442b1498231197a0e
hiddenmonkey <boogaloofa@gmail.com>
2013-04-03 12:27:14 -0700

    Create Main.java
    A sample GUI, for a create account screen (functionality to come)

---
+++ Main.java
@@ -0,0 +1,159 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.JSpinner;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Font;
+import java.awt.Color;
+
+
+public class Main extends JFrame {
+
+  private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblPin;
+	private JTextField textField_1;
+	private JLabel lblUserType;
+	private JComboBox comboBox;
+	private JLabel lblNewLabel;
+	private JLabel lblnoSpaces;
+	private JLabel lblDigits;
+	private JButton btnNewButton;
+	public LoginList userDatabase = new LoginList(); 
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{52, 81, 142, 0, 0, 0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{25, 20, 20, 20, 0, 0, 0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		lblNewLabel = new JLabel("Create Account");
+		lblNewLabel.setBackground(Color.MAGENTA);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridwidth = 2;
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblUserName = new JLabel("User Name");
+		GridBagConstraints gbc_lblUserName = new GridBagConstraints();
+		gbc_lblUserName.anchor = GridBagConstraints.EAST;
+		gbc_lblUserName.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUserName.gridx = 0;
+		gbc_lblUserName.gridy = 2;
+		contentPane.add(lblUserName, gbc_lblUserName);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 2;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		lblnoSpaces = new JLabel("(no spaces)");
+		GridBagConstraints gbc_lblnoSpaces = new GridBagConstraints();
+		gbc_lblnoSpaces.anchor = GridBagConstraints.WEST;
+		gbc_lblnoSpaces.insets = new Insets(0, 0, 5, 5);
+		gbc_lblnoSpaces.gridx = 2;
+		gbc_lblnoSpaces.gridy = 2;
+		contentPane.add(lblnoSpaces, gbc_lblnoSpaces);
+		
+		lblPin = new JLabel("Pin");
+		GridBagConstraints gbc_lblPin = new GridBagConstraints();
+		gbc_lblPin.anchor = GridBagConstraints.EAST;
+		gbc_lblPin.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPin.gridx = 0;
+		gbc_lblPin.gridy = 3;
+		contentPane.add(lblPin, gbc_lblPin);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 3;
+		contentPane.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		lblDigits = new JLabel("(4 digits 0-9)");
+		GridBagConstraints gbc_lblDigits = new GridBagConstraints();
+		gbc_lblDigits.anchor = GridBagConstraints.WEST;
+		gbc_lblDigits.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDigits.gridx = 2;
+		gbc_lblDigits.gridy = 3;
+		contentPane.add(lblDigits, gbc_lblDigits);
+		
+		lblUserType = new JLabel("User Type");
+		GridBagConstraints gbc_lblUserType = new GridBagConstraints();
+		gbc_lblUserType.anchor = GridBagConstraints.NORTHEAST;
+		gbc_lblUserType.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUserType.gridx = 0;
+		gbc_lblUserType.gridy = 4;
+		contentPane.add(lblUserType, gbc_lblUserType);
+		
+		comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Doctor", "Nurse", "Patient"}));
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.anchor = GridBagConstraints.NORTH;
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 4;
+		contentPane.add(comboBox, gbc_comboBox);
+		
+		btnNewButton = new JButton("Create");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 6;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+	}
+
+}
