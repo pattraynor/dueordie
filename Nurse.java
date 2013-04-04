164b07b33ccd1332b12a1fe47fce1312ffc8c4eb
DrHarbinger <code.matthew@gmail.com>
2013-04-03 23:59:17 -0700

    Update Nurse.java
    implements createNewAccount and changed constructor

--- Nurse.java
+++ Nurse.java
@@ -1,13 +1,47 @@
-public class Nurse extends User {
-    public Nurse(){
+/**
+ * Author: Matthew
+ * Date: 4/3/13
+ */
+public class Nurse extends User
+{
+
+    public Nurse(String setName, int setPin)
+    {
+        super(setName, setPin);
         userType = "Nurse";
-    }    
-    public newVisit(int patientID){
-        
+        answer1 = "";
+        answer2 = "";
+        answer3 = "";
+
     }
-    
-    public createNewAccount(String userName, String userType){
-        
+
+    public Visit newVisit(int patientID)
+    {
+        Visit _newVisit = new Visit();
+        return _newVisit;
+
     }
-    
+
+    public User createNewAccount(String _userName, String userType, int setPin)
+    {
+        if (userType == "Nurse")
+        {
+            Nurse newNurse = new Nurse(_userName, setPin);
+            return newNurse;
+
+        }
+        if (userType == "Doctor")
+        {
+            Nurse newDoctor = new Doctor(_userName, setPin);
+            return newDoctor;
+        }
+        if (userType == "Patient")
+        {
+            Patient newPatient= new Patient(_userName, setPin);
+            return newPatient;
+        }
+        return null;
+    }
+
+
 }
