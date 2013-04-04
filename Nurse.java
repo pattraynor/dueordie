64ab676faf6ed05154e790be0eb1555132337632
HIGHonLIFE1012 <highonlife1012@yahoo.com>
2013-04-03 23:56:56 -0700

    Create Nurse.java

---
+++ Nurse.java
@@ -0,0 +1,13 @@
+public class Nurse extends User {
+    public Nurse(){
+        userType = "Nurse";
+    }    
+    public newVisit(int patientID){
+        
+    }
+    
+    public createNewAccount(String userName, String userType){
+        
+    }
+    
+}
