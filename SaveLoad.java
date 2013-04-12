efb1a9ecc7c1cc6fb8f1de1123722db55cdb714c
pattraynor <ptraynor@asu.edu>
2013-04-11 21:41:36 -0700

    SaveLoad

---
+++ SaveLoad.java
@@ -0,0 +1,28 @@
+//Should save to a file, and then load the file from a previous session.
+import java.io.FileOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+public class SaveLoad 
+{
+	public SaveLoad()
+	{
+		
+		
+		
+		
+	}
+	
+	 public int save()
+	 {
+	         return 0;
+     }
+
+	    //-------------------------------------------------
+	    // loads LoginList and all user information from file
+	    // -------------------------------------------------
+	    public int load()
+	    {
+	         return 0;
+	    }
+
+}
