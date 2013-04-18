790f5adfd894f65c43b9b742a26c9c8f1c6e3b1f
hiddenmonkey <boogaloofa@gmail.com>
2013-04-17 22:50:57 -0700

    Update Globals.java
    Added chart specific globals

--- Globals.java
+++ Globals.java
@@ -1,12 +1,23 @@
-
+//all global variables that are used by program 
 public class Globals {
 	
 	public static LoginList userDatabase = new LoginList();
+	public static InputHandler inputHandler = new InputHandler();
+	public static FileHandler fileHandler = new FileHandler();
 	public static final String PATIENT = "Patient";
 	public static final String DOCTOR = "Doctor";
 	public static final String NURSE = "Nurse";
+	public static final String WEIGHT = "Weight";
+	public static final String HEIGHT = "Height";
+	public static final String BLOODPRESSURE = "Blood Pressure";
+	public static final String HEIGHTWEIGHT = "Height vs Weight";
+	public static final String BLOODSUGAR = "Blood Sugar";
+	public static final String LINECHART = "Line Chart";
+	public static final String BARCHART = "BarChart";
+	public static final String BARCHART3D = "BarChart3D";
 	public static final String QUESTION1 = "Where did you attend HighSchool?";
 	public static final String QUESTION2 = "What is your favorite animal?";
 	public static final String QUESTION3 = "What is your mother's maiden name?";
+	
 
 }
